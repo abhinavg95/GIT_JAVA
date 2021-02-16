d2bc82421b7a76d421a1a11925ce617f07271d4c
Abhinav <abhinavg1000@gmail.com>
2021-02-16 19:14:18 +0530

    made some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("This is a java class that ");
+        System.out.println("This is a java class that will help us get started");
+        String a="Abhinav";
+        System.out.println(a);
     }
 }
